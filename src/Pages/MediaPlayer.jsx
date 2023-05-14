2c4dc0ca2749ffbed975cac67789e37b87e4f055
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-05-14 16:58:25 +0530

    Completed Medai Player Component Code

--- src/Pages/MediaPlayer.jsx
+++ src/Pages/MediaPlayer.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useRef } from "react";
-import { Box, Flex, Heading, Icon, Image, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Icon, Image, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Text } from "@chakra-ui/react";
 import {RiPauseCircleFill, RiPlayCircleFill} from 'react-icons/ri';
 import skipForward from '../Images/skipForward.svg';
 import skipBackward from '../Images/skipBackward.svg';
 import Vector from '../Images/Vector.svg';
 import Previous from '../Images/Previous.svg';
 import Next from '../Images/Next.svg';
+import Book from '../Images/Book.svg';
 import {ChevronDownIcon} from '@chakra-ui/icons';
 import {songsList} from '../Songs/songsDB';
 
@@ -77,17 +78,19 @@ function MediaPlayer({id=0}) {
     <Box>
         <Flex w={'full'} justifyContent={'space-between'} alignItems={"center"} mb={6}>
             <Icon fontSize='30px' as={ChevronDownIcon} color={'white'} fontWeight={"extrabold"} />
-            <Image src={Vector} />
+            <Image src={Vector}  alt="vector_icon" />
         </Flex>
 
         {/* Song Details Section */}
+
         <Flex 
             justifyContent={'center'} 
             alignItems={"center"} 
             mb={'16px'}   
             >
             <Image 
-                src={songsList[nowPlayingIndex].songImg} 
+                src={songsList[nowPlayingIndex].songImg}
+                alt={songsList[nowPlayingIndex].songName}
                 w={'326px'} 
                 h={'132px'} 
                 boxShadow= '0px 1px 4px rgba(0, 0, 0, 0.1)'
@@ -96,6 +99,33 @@ function MediaPlayer({id=0}) {
         </Flex>
         <Heading mb='8px' textAlign='center' color='white' fontSize={'20px'}>{songsList[nowPlayingIndex].songName}</Heading>
         <Text textAlign='center' color='white' opacity={0.6} mb={4}>{songsList[nowPlayingIndex].artist}</Text>
+        <Flex flexDir="column" gap='8px' mb='75px'>
+            <Button 
+                m='auto' 
+                colorScheme="blackAlpha" 
+                size='sm' 
+                w='220px' 
+                h='32px'
+                bgColor='#2E3035' 
+                fontWeight='light'
+            >
+                View Chapter Notes
+            </Button>
+            <Button 
+                m='auto' 
+                colorScheme="blackAlpha" 
+                size='sm' 
+                w='220px' 
+                h='32px'
+                bgColor='#2E3035' 
+                fontWeight='light'
+            >
+                Attempt Quiz
+            </Button>
+        </Flex>
+        
+        {/* Media Player Code */}
+
         <Slider defaultValue={0} max={audioRef.current?.duration} value={audioRef?.current?.currentTime} onChange={(time) => audioRef.current.currentTime!==null ? audioRef.current.currentTime = time : 0}>
             <SliderTrack bg={'#2E3035'} >
                 <SliderFilledTrack bg={'#3281FF'} />
@@ -112,11 +142,16 @@ function MediaPlayer({id=0}) {
             onTimeUpdate={() => setCurrentTime(audioRef.current?.currentTime)}
         ></audio>
         <Flex w='full' justifyContent={'space-between'} alignItems={'center'} my={'42px'}>
-            <Image src={Previous} onClick={previous} w='32px' />
-            <Image src={skipBackward} onClick={skip15SecBackward} w='32px' />
+            <Image src={Previous} alt="previous_icon" onClick={previous} w='32px' />
+            <Image src={skipBackward} alt="skip_backward_icon" onClick={skip15SecBackward} w='32px' />
             <Icon color='white' fontSize='72px' as={nowPlaying ? RiPauseCircleFill : RiPlayCircleFill} onClick={nowPlaying ? pause : play} />
-            <Image src={skipForward} onClick={skip15SecForward}  w='32px' />
-            <Image src={Next} onClick={next}  w='32px' />
+            <Image src={skipForward} alt="skip_forward_icon" onClick={skip15SecForward}  w='32px' />
+            <Image src={Next} alt="next_icon" onClick={next}  w='32px' />
+        </Flex>
+
+        <Flex justifyContent={'center'} alignItems={"center"} gap='8px' mt={'106px'} mb={'16px'}>
+            <Image src={Book} alt="book_icon" w='22px' />
+            <Text color='white' opacity={0.6}>Chapters</Text>
         </Flex>
     </Box>
   );
